8bb1f57f06389d37eff1bcf81e0fea7ae2308d2d
your name here <pqpq.bkbk3t3@gmail.com>
2020-08-16 10:50:20 -0400

    added demo login on side bar

--- src/app/problems/pipes/filter.pipe.ts
+++ src/app/problems/pipes/filter.pipe.ts
@@ -3,15 +3,12 @@ import { Pipe, PipeTransform } from "@angular/core";
   name: "filter",
 })
 export class FilterPipe implements PipeTransform {
-  // debugger
   transform(items: any[], searchText: string): any[] {
-    // debugger
 
     if (!items) return [];
     if (!searchText) return items;
     searchText = searchText.toLowerCase();
     return items.filter((it) => {
-      console.log("it", it)
       return it.title.toLowerCase().includes(searchText);
     });
   }
